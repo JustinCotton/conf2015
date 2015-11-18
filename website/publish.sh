4b52d1320c1ec364093cc5b0700c75a93aba7966
Christopher Chedeau <vjeux@fb.com>
2015-11-18 13:29:58 -0800

    2014 -> 2015

--- website/publish.sh
+++ website/publish.sh
@@ -5,18 +5,18 @@ set -ex
 # Start in website/ even if run from root directory
 cd "$(dirname "$0")"
 
-cd ../../conf2014-gh-pages
+cd ../../conf2015-gh-pages
 git checkout -- .
 git clean -dfx
 git fetch
 git rebase
 rm -Rf -- *
-cd ../conf2014/website
+cd ../conf2015/website
 node server/generate.js
-cp -R build/conf2014/* ../../conf2014-gh-pages/
+cp -R build/conf2015/* ../../conf2015-gh-pages/
 rm -Rf build/
-cd ../../conf2014-gh-pages
+cd ../../conf2015-gh-pages
 git add --all
 git commit -m "update website"
 git push
-cd ../conf2014/website
+cd ../conf2015/website
